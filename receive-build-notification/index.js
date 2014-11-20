b745a8094e5acf882f0dd7186c301e256d5157f8
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2014-11-20 08:09:23 -0500

    Better messages, less frequently

--- receive-build-notification/index.js
+++ receive-build-notification/index.js
@@ -6,19 +6,20 @@ var sevaurl = process.env.SEVA_URL || "http://localhost:5000/message_unsigned/";
 var chatid = process.env.SEVA_CHAT || "a-chat-id";
 var decode = require('querystring').parse;
 
-var messageForStatus = [ "Passed", "Fixed", "Broken", "Still Failing" ];
+var messageForStatus = [ "Fixed", "Broken", "Still Failing" ]; // not "Passed"
 var failedStatus = [ "Broken", "Still Failing" ];
 
 var server = http.createServer(function (req, response) {
 
   var auth = req.headers.authorization && req.headers.authorization.trim();
   
+  console.log('Build notification', { url: req.url, 'head': req.headers});
   if (req.url  !== "/notify/status" || auth !== secret){
     console.log('Bad secret on build notification');
     response.statusCode = 401;
     return response.end();
   }
-  console.log('Build notification', { url: req.url });
+
   var data = '';
   req.on('data', function (chunk) {
     data += chunk;
@@ -27,12 +28,18 @@ var server = http.createServer(function (req, response) {
     var form = decode(data);
     var n = JSON.parse(form && form.payload);
     console.log("msg", n.status_message);
+    var author = n.author_email.split("@")[0]; 
+    var svnrev = n.message.match(/(@\d{3,})/g);
+    svnrev = svnrev && svnrev[0] || "??";
+
     if (messageForStatus.indexOf(n.status_message) !== -1) {
       var emote = ":-)";
       if (failedStatus.indexOf(n.status_message) !== -1) {
         emote = ":-(";
       }
-      var message = "Build status: " + n.status_message + " " + emote + " For commit by " + n.author_email ;
+      var message = "Build status: " + n.status_message + " " +
+                    emote + " For commit " + svnrev + " by " + author;
+
       console.log("Messaging: " , message);
       request.post(sevaurl, {form:{chat_id:chatid, message: message}}, function(){
         response.end();
